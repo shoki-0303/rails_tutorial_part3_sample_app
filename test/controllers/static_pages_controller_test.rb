f580c5523d85db4a0136d371ad12b7b455cd7c3b
shoki-0303 <shoki522.33333@gmail.com>
2019-01-05 05:15:51 +0900

    refactor

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -9,7 +9,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   # テストの内容は
   # Homeページのテスト。Getリクエストをhomeアクションに対して送れば、リクエストのレスポンスは200のはず。
   test "should get home" do
-    get static_pages_home_url
+    get root_url
     assert_response :success
     assert_select "title", "Home | #{@base_title}"
   end
