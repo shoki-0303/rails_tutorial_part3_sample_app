2fb4d150aa090a6fc7bdfe6839aeb9f30f305ee3
shoki-0303 <shoki522.33333@gmail.com>
2019-01-12 12:12:34 +0900

    db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_11_023754) do
+ActiveRecord::Schema.define(version: 2019_01_12_031018) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
