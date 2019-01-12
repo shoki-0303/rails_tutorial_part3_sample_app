1a4020a1373487fe477f69fd42fb366092441887
shoki-0303 <shoki522.33333@gmail.com>
2019-01-12 11:52:56 +0900

    set uniqueness validation for email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
   validates :name, presence: true, length: {maximum: 50}
   VALID_EMAIL_REGEXP = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, length: {maximum: 251}, format: { with: VALID_EMAIL_REGEXP}
+  validates :email, presence: true, length: {maximum: 251}, format: { with: VALID_EMAIL_REGEXP}, uniqueness: true
 end
