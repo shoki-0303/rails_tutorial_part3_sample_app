35d29023b710980b71ea444c4a01c00826c9b31f
shoki-0303 <shoki522.33333@gmail.com>
2019-01-12 12:27:50 +0900

    email become lower-case before save

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  before_save { self.email = email.downcase }
   validates :name, presence: true, length: {maximum: 50}
   VALID_EMAIL_REGEXP = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: {maximum: 251}, format: { with: VALID_EMAIL_REGEXP}, uniqueness: { case_sensitive: false }
