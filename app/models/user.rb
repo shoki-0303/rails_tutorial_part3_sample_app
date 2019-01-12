7a2d9f3eb2e68b68f7d6c8b349355abc5aa3ee36
shoki-0303 <shoki522.33333@gmail.com>
2019-01-12 22:36:38 +0900

    set has_secure_password

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ApplicationRecord
   validates :name, presence: true, length: {maximum: 50}
   VALID_EMAIL_REGEXP = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: {maximum: 251}, format: { with: VALID_EMAIL_REGEXP}, uniqueness: { case_sensitive: false }
+  has_secure_password
 end
