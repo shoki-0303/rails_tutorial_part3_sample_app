c3a5278dd96c0591b0248fec103ce99ccfce5bac
shoki-0303 <shoki522.33333@gmail.com>
2019-01-14 14:00:47 +0900

    set user_params method in private

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,10 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
 end
