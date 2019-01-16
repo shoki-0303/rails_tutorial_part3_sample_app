ffd52e28db63636caa583eb1946dc5f84f47b8b4
shoki-0303 <shoki522.33333@gmail.com>
2019-01-16 20:30:23 +0900

    Fix flash in create method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
     if !!(user && user.authenticate(params[:session][:password]))
       # ログインに成功した際の処理を書く
     else
-      flash[:danger] = "Invalid email/password combination"
+      flash.now[:danger] = "Invalid email/password combination"
       render 'new'
     end
   end
