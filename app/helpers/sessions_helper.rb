8f0f68350deaac898f38a80847c4bcec7c33dcd5
shoki-0303 <shoki522.33333@gmail.com>
2019-01-16 20:42:57 +0900

    create log_in method in SessionHelpers Module

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,9 @@
 module SessionsHelper
+  # 渡されたユーザーでログインする
+  # railsで事前定義されているsessionメソッドを使用
+  # 一時cookiesに暗号化済みのユーザーIDが設定される
+  # 一時cookiesはブラウザが閉じられると期限がきれる
+  def log_in(user)
+    session[:user_id] = user.id
+  end
 end
