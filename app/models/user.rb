c6d6448d60a32133997e0e65eb929016ce868d8d
shoki-0303 <shoki522.33333@gmail.com>
2019-01-21 00:10:41 +0900

    Create digest class method

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,10 @@ class User < ApplicationRecord
   validates :email, presence: true, length: {maximum: 251}, format: { with: VALID_EMAIL_REGEXP}, uniqueness: { case_sensitive: false }
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
+
+  #渡された値のハッシュ値を返す
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
 end
